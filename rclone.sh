404df328629b536ddfc5d0806ee1b4516719f972
Dedsec1 <Dedsec1@users.noreply.github.com>
2017-06-21 00:40:13 -0400

    Update rclone.sh

--- rclone.sh
+++ rclone.sh
@@ -7,6 +7,6 @@
 repo=$1
 regex=${2:-gz}
 
-wget -qO- https://api.github.com/repos/$repo/releases/latest | \
+wget -qO- https://api.github.com/repos/$repo/releases/latest >/dev/null 2>&1 | \
   awk -v regex="$regex" -F '"' '/browser_download_url/ && match($4, regex) { print $4 }' | \
   xargs wget
